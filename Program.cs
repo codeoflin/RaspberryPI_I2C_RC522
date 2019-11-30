1853b40f2f4ec4d0f9da986c5a30bebd04a8d8a6
codeoflinz <h@hhh.im>
2019-11-30 17:28:54 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Device.I2c;
+
+namespace RaspberryPI_I2C_RC522
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
